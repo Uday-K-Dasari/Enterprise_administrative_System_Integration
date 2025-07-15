f1236449e39422ffd4ff7ca613dc2a279ca27767
Uday kumar Dasari <108976321+Uday-K-Dasari@users.noreply.github.com>
2025-07-15 12:08:28 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Enterprise_administrative_System_Integration",
+  //base: process.env.VITE_BASE_PATH || "/Enterprise_administrative_System_Integration",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
